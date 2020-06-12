8f7a2fbe11c3ccd213230f22896a17e9fa9a892f
VladislavYaKo <64699989+VladislavYaKo@users.noreply.github.com>
2020-06-12 20:07:00 +0300

    Add files via upload

---
+++ OopLabs3_Part1/DeflateCompressor/DeflateCompressor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginsInterface;
+
+namespace Plugins
+{
+    public class DeflateCompressor : IPlugin
+    {
+        public MemoryStream Compress(byte[] inputData)
+        {
+            MemoryStream outputStream = new MemoryStream();
+            MemoryStream inputStream = new MemoryStream(inputData);
+            using (DeflateStream compressionStream = new DeflateStream(outputStream, CompressionMode.Compress))
+            {
+                inputStream.CopyTo(compressionStream);
+            }
+                return outputStream;
+        }
+
+        public byte[] Decompress(MemoryStream compressedStream)
+        {
+            MemoryStream outputStream = new MemoryStream();
+            using(DeflateStream decompressedStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+            {
+                decompressedStream.CopyTo(outputStream);
+            }
+            return outputStream.ToArray();
+        }
+    }
+}
